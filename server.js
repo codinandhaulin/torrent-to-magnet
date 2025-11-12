8a3e3ca04e85ad9b341aeccbe9cf2b3cd95bfd68
Martin Bradley <mbradley@codematters.com>
2025-11-12 13:14:47 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Allow CORS so the frontend on GitHub Pages can call this backend
 app.use((req, res, next) => {
